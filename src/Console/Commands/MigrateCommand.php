50287105e7f6cc074a978db053ca6e4003a7e113
Abdelrahman Omran <me@omranic.com>
2020-03-18 15:07:33 +0400

    Add shortcut -f (force) for artisan publish commands

--- src/Console/Commands/MigrateCommand.php
+++ src/Console/Commands/MigrateCommand.php
@@ -13,7 +13,7 @@ class MigrateCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'rinvex:migrate:forms {--force : Force the operation to run when in production.}';
+    protected $signature = 'rinvex:migrate:forms {--f|force : Force the operation to run when in production.}';
 
     /**
      * The console command description.
