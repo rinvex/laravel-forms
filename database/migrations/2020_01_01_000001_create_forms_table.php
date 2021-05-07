667d6ebcbb0c12c00e7fabc905be827f01cfff5f
Abdelrahman Omran <me@omranic.com>
2021-05-07 07:48:10 +0400

    Drop old MySQL versions support that doesn't support json columns

--- database/migrations/2020_01_01_000001_create_forms_table.php
+++ database/migrations/2020_01_01_000001_create_forms_table.php
@@ -20,11 +20,11 @@ class CreateFormsTable extends Migration
             $table->increments('id');
             $table->nullableMorphs('entity');
             $table->string('slug');
-            $table->{$this->jsonable()}('name');
-            $table->{$this->jsonable()}('description')->nullable();
-            $table->{$this->jsonable()}('content');
-            $table->{$this->jsonable()}('actions')->nullable();
-            $table->{$this->jsonable()}('submission')->nullable();
+            $table->json('name');
+            $table->json('description')->nullable();
+            $table->json('content');
+            $table->json('actions')->nullable();
+            $table->json('submission')->nullable();
             $table->boolean('is_active')->default(false);
             $table->boolean('is_public')->default(false);
             $table->timestamps();
@@ -43,19 +43,5 @@ class CreateFormsTable extends Migration
     public function down()
     {
         Schema::dropIfExists(config('rinvex.forms.tables.forms'));
-    }
-
-    /**
-     * Get jsonable column data type.
-     *
-     * @return string
-     */
-    protected function jsonable(): string
-    {
-        $driverName = DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME);
-        $dbVersion = DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
-        $isOldVersion = version_compare($dbVersion, '5.7.8', 'lt');
-
-        return $driverName === 'mysql' && $isOldVersion ? 'text' : 'json';
     }
 }
--- database/migrations/2020_01_01_000002_create_form_responses_table.php
+++ database/migrations/2020_01_01_000002_create_form_responses_table.php
@@ -19,7 +19,7 @@ class CreateFormResponsesTable extends Migration
             // Columns
             $table->increments('id');
             $table->string('unique_identifier')->nullable();
-            $table->{$this->jsonable()}('content');
+            $table->json('content');
             $table->integer('form_id')->unsigned();
             $table->nullableMorphs('user');
             $table->timestamps();
@@ -39,19 +39,5 @@ class CreateFormResponsesTable extends Migration
     public function down()
     {
         Schema::dropIfExists(config('rinvex.forms.tables.form_responses'));
-    }
-
-    /**
-     * Get jsonable column data type.
-     *
-     * @return string
-     */
-    protected function jsonable(): string
-    {
-        $driverName = DB::connection()->getPdo()->getAttribute(PDO::ATTR_DRIVER_NAME);
-        $dbVersion = DB::connection()->getPdo()->getAttribute(PDO::ATTR_SERVER_VERSION);
-        $isOldVersion = version_compare($dbVersion, '5.7.8', 'lt');
-
-        return $driverName === 'mysql' && $isOldVersion ? 'text' : 'json';
     }
 }
